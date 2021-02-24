916b85ef2b45832f081b6fcd9c6eed4925135fa5
Saïd Redouane <s_red009@hotmail.com>
2021-02-24 00:33:43 +0000

    Add comments to increase readability

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,9 +2,11 @@ $(document).ready(function() {
   $("#tweet-text").on("input", function(Event) {
     const charCounter = this.value.length;
     const charRemaining = 140 - charCounter;
+    //Select the output element and change its text to charRemaining
     let counterElement = $(Event.target).next("#under-text").find(".counter");
     counterElement.text(charRemaining);
 
+    //If the number of characters exceeds 140 then turn the counter to red
     if (charRemaining < 0) {
       counterElement.css("color", "red");
     } else {
