6e3f24010ada62547c7ea0956dda5803eb9d70ed
Saïd Redouane <s_red009@hotmail.com>
2021-02-24 00:30:11 +0000

    Add the logic that makes the counter dynamic

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,14 @@
-alert("hello");
+$(document).ready(function() {
+  $("#tweet-text").on("input", function(Event) {
+    const charCounter = this.value.length;
+    const charRemaining = 140 - charCounter;
+    let counterElement = $(Event.target).next("#under-text").find(".counter");
+    counterElement.text(charRemaining);
+
+    if (charRemaining < 0) {
+      counterElement.css("color", "red");
+    } else {
+      counterElement.css("color", "#4056a1");
+    }
+  });
+});
